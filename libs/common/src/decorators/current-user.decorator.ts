99c54395dae0629a3d5a2884fac8cc79c4b2a5d9
lucaskampi <lucasclmaia0@gmail.com>
2026-04-09 16:35:02 -0300

    feat(common): add guards, filters, and common module

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,8 +1,20 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+
+export interface RequestUser {
+  id: string;
+  [key: string]: any;
+}
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
+  (data: keyof RequestUser | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user as RequestUser;
+    return data ? user?.[data] : user;
   },
 );
+
+export const Public = () => {
+  const { SetMetadata } = require('@nestjs/common');
+  return SetMetadata('isPublic', true);
+};
