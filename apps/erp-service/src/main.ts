2d0fa851a2508ea2a3a0b86cbe3bd1de70fab801
lucaskampi <lucasclmaia0@gmail.com>
2026-04-09 16:10:36 -0300

    feat(menu-service): scaffold service structure

---
+++ apps/erp-service/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('ERP Service')
+    .setDescription('ERP Service API for QR Food')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3004);
+  console.log('ERP Service running on http://localhost:3004/api');
+}
+bootstrap();
